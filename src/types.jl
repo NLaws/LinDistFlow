2d13a7d8a245a65bb759f0191c2b17889610ed4a
Nick Laws <laws.nick@gmail.com>
2021-12-29 07:42:50 -0700

    add singlephase38linesInputs to types.jl
    convenience function for creating network model

--- src/types.jl
+++ src/types.jl
@@ -155,3 +155,52 @@ function Inputs(
         Q_lo_bound=Q_lo_bound,
     )
 end
+
+
+"""
+    singlephase38linesInputs(;
+        Pload=Dict{String, AbstractArray{Real, 1}}(), 
+        Qload=Dict{String, AbstractArray{Real, 1}}(), 
+        T=24,
+        loadnodes = ["3", "5", "36", "9", "10", "11", "12", "13", "15", "17", "18", "19", "22", "25", 
+                    "27", "28", "30", "31", "32", "33", "34", "35"],
+        Sbase = 1e6,
+        Vbase = 12.5e3,
+        v0=1.0,
+        v_uplim = 1.05,
+        v_lolim = 0.95,
+    )
+
+Convenience function for creating a single phase network with 38 lines and nodes. 
+Taken from:
+Andrianesis et al. 2019 "Locational Marginal Value of Distributed Energy Resources as Non-Wires Alternatives"
+
+NOTE that Inputs is a mutable struct (s.t. loads can be added later).
+"""
+function singlephase38linesInputs(;
+    Pload=Dict{String, AbstractArray{Real, 1}}(), 
+    Qload=Dict{String, AbstractArray{Real, 1}}(), 
+    T=24,
+    loadnodes = ["3", "5", "36", "9", "10", "11", "12", "13", "15", "17", "18", "19", "22", "25", 
+                 "27", "28", "30", "31", "32", "33", "34", "35"],
+    Sbase = 1e6,
+    Vbase = 12.5e3,
+    v0=1.0,
+    v_uplim = 1.05,
+    v_lolim = 0.95,
+    )
+    folderpath = joinpath(dirname(@__FILE__), "..", "test", "data")
+    Inputs(
+        joinpath(folderpath, "singlephase38lines.dss"), 
+        "0", 
+        joinpath(folderpath, "singlephase38linecodes.dss");
+        Pload=Pload, 
+        Qload=Qload,
+        Sbase=Sbase, 
+        Vbase=Vbase, 
+        v0 = v0,
+        v_uplim = v_uplim,
+        v_lolim = v_lolim,
+        Ntimesteps = T
+    )
+end
