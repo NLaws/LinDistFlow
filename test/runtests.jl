f60521906e71b0ee4a14118abe017dd61916f8dd
Nick Laws <laws.nick@gmail.com>
2022-06-06 21:23:27 -0600

    WiP in comparing Qvar to Arnold 2016

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using Random
 using LinDistFlow
 using HiGHS
 using JuMP
+using Ipopt
 Random.seed!(42)
 
 
@@ -42,7 +43,17 @@ Random.seed!(42)
 end
 
 
+# TODO https://github.com/lanl-ansi/PowerModelsDistribution.jl/blob/926b5acb6bf13231191500d404008cda580f240d/src/io/dss/dss_parse.jl#L807 fails in Ubuntu Actions tests
+
 @testset "three phase openDSS" begin
+    #=
+    Validating against Arnold 2016 results, which requires:
+    - dividing loads by 10? (according to values in Table I)
+    - increasing impedances by 1.25 (Section IV, paragraph 3)
+    - TODO? use equ.s (3) & (4) for voltage-dependent loads
+    - allowing decisions for reactive power injection at busses 632, 675, 680, and 684
+    - remove the "distributed load" on bus 670  (removing this load makes all Qvar ≈ 0 ???)
+    =#
     p = Inputs(
         "data/13bus/IEEE13Nodeckt.dss", 
         "rg60";
@@ -51,25 +62,73 @@ end
         Sbase=500000, # get Sbase, Vbase from openDSS ?
         Vbase=4160, 
         v0 = 1.00,
-        v_uplim = 1.1,
-        v_lolim = 0.9,
+        v_uplim = 1.05,
+        v_lolim = 0.95,
         Ntimesteps = 1,
-        P_up_bound=1e4,
-        Q_up_bound=1e4,
-        P_lo_bound=-1e4,
-        Q_lo_bound=-1e4,
+        P_up_bound=1e5,
+        Q_up_bound=1e5,
+        P_lo_bound=-1e5,
+        Q_lo_bound=-1e5,
     );
-    # TODO https://github.com/lanl-ansi/PowerModelsDistribution.jl/blob/926b5acb6bf13231191500d404008cda580f240d/src/io/dss/dss_parse.jl#L807 fails in Ubuntu Actions tests
+    p.Qresource_nodes = ["632", "675", "680", "684"]
+    for linecode in keys(p.Zdict)
+        p.Zdict[linecode]["rmatrix"] *= 1.25
+        p.Zdict[linecode]["xmatrix"] *= 1.25
+    end
+    delete!(p.Pload, "670")
+    delete!(p.Qload, "670")
+    p.Pload["633"] = p.Pload["634"]  # "ignoring the transformer between 633 and 644
+    p.Qload["633"] = p.Qload["634"]  # "ignoring the transformer between 633 and 644
+    delete!(p.Pload, "634")
+    delete!(p.Qload, "634")
+    # for b in keys(p.Pload), phs in keys(p.Pload[b])
+    #     p.Pload[b][phs] /= 10
+    # end
+    # for b in keys(p.Qload), phs in keys(p.Qload[b])
+    #     p.Qload[b][phs] /= 10
+    # end
+
     @test typeof(p) == LinDistFlow.Inputs{LinDistFlow.ThreePhase}
 
-    m = Model(HiGHS.Optimizer)
+    m = Model(Ipopt.Optimizer)
     build_ldf!(m, p)
+    receiving_busses = collect(e[2] for e in p.edges)
+    phases_into_bus = Dict(k=>v for (k,v) in zip(receiving_busses, p.phases))  # TODO add this to Inputs
+    
+    @objective(m, Min,
+        0.5 * sum( m[:Qvar][b][phs][1]^2 for b in p.Qresource_nodes, phs in phases_into_bus[b]) +
+        sum(
+            (m[:vsqrd][b,phs1,1] - m[:vsqrd][b,phs2,1])^2
+            for b in setdiff(p.busses, [p.substation_bus]), 
+                (phs1, phs2) in [[1,2], [1,3], [2,3]] if phs1 in phases_into_bus[b] && phs2 in phases_into_bus[b]
+        )
+    )
 
-    @objective(m, Min, sum(m[:Pⱼ][p.substation_bus, phs, t] + m[:Qⱼ][p.substation_bus, phs, t] for phs in 1:3, t in 1:p.Ntimesteps))
+
+    # @objective(m, Min, sum(m[:Pⱼ][p.substation_bus, phs, t] + m[:Qⱼ][p.substation_bus, phs, t] for phs in 1:3, t in 1:p.Ntimesteps))
     optimize!(m)
-    @test termination_status(m) == MOI.OPTIMAL
+    @test termination_status(m) == MOI.LOCALLY_SOLVED
+
+    phsa,phsb,phsc = 0,0,0
+    for b in p.Qresource_nodes, phs in phases_into_bus[b], t in 1:1
+        println(b, ".", phs, " ", round(value(m[:Qvar][b][phs][t])/p.Sbase, digits=4) )
+        if phs == 1
+            phsa += value(m[:Qvar][b][phs][t])/p.Sbase
+        elseif phs == 2
+            phsb += value(m[:Qvar][b][phs][t])/p.Sbase
+        elseif phs == 3
+            phsc += value(m[:Qvar][b][phs][t])/p.Sbase
+        end
+    end
+    
+    #=
+    Not getting same values as Arnold for Qvar.
+    Phase C has voltages at 0.95 in my solution but Arnold's lowest phase C voltages are near 0.97
+    (Phase C is the only phase that will violate 0.95 without Qvar)
+    =#
 
     vsqrd = get_bus_values("vsqrd", m, p)
     vmag = Dict(k => sqrt(v) for (k,v) in vsqrd)
+
     # TODO vmag tests? or implement var control a-la Arnold 2016; could compare to PowerModelsDistribution
 end
