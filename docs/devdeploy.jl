db5bc4bdd5e65e1b9b382410749666c4b2a162a8
Nick Laws <laws.nick@gmail.com>
2022-06-11 21:30:01 -0600

    rm docs/Manifest.toml (b/c you have to dev the package in docs env)
    to pick up on new doc strings

--- docs/devdeploy.jl
+++ docs/devdeploy.jl
@@ -1,5 +1,8 @@
 using LiveServer
 
+"""
+NOTE you have to dev LinDistFlow in the docs environment to get local changes
+"""
 function devbuildserve()
     rm("build", force=true, recursive=true)
     include("make.jl")
